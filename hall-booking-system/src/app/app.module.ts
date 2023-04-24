451b5e3069f0769b4e0d294e720effd76de98d34
MynthanKumar7 <84435156+MynthanKumar7@users.noreply.github.com>
2023-04-24 14:11:04 +0530

    bootstrap and angular material installed.
    New components added

--- hall-booking-system/src/app/app.module.ts
+++ hall-booking-system/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HallListComponent } from './hall-list/hall-list.component';
+import { ConfirmBookingComponent } from './confirm-booking/confirm-booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HallListComponent,
+    ConfirmBookingComponent
   ],
   imports: [
     BrowserModule,
